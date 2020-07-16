b9338448eaabbe25eee9006aafd65b1eba3613e7
saifur703 <saifur703@gmail.com>
2020-07-16 11:57:07 +0600

    quick start pack

---
+++ assets/js/active.js
@@ -0,0 +1,11 @@
+(function ($) {
+  'use strict';
+
+  jQuery(document).ready(function ($) {
+    // code here
+  });
+
+  jQuery(window).on('load', function () {
+    // code here
+  });
+})(jQuery);
